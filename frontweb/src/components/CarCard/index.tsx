46ed913868b37182da2c7674d3ec4402592b85cd
claytoncalixto <claytonrcalixto@gmail.com>
2021-12-16 11:04:09 -0300

    Created grid system catalog - part 3

--- frontweb/src/components/CarCard/index.tsx
+++ frontweb/src/components/CarCard/index.tsx
@@ -2,13 +2,11 @@ import CarImg from 'assets/images/car_card_2.png';
 import ButtonBuy from 'components/ButtonBuy';
 import './styles.css';
 
+
 const CarCard = () => {
-  return (
-    <>
-      <div className="car-top-container">       
-          <input className="form-control mr-sm-2" type="search" placeholder="Digite sua busca" aria-label="Search"  ></input>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit"> Buscar </button>        
-      </div>
+  
+  return (    
+    <>      
       <div className="car-bottom-container">
         <img src={CarImg} alt="Nome do carro" />
         <h6>Audi Supra TT</h6>
--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,11 +1,16 @@
 import CarCard from 'components/CarCard';
 import Navbar from 'components/Navbar';
+import './styles.css';
 
 const Catalog = () => {
   return (
     <>
       <Navbar />
-      <div className="container my-4">
+      <div className="car-top-container">
+          <input  className="form-control mr-sm-2" type="search" placeholder="Digite sua busca" aria-label="Search" ></input>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit"> {' '} Buscar{' '} </button>
+      </div>
+      <div className="container my-4">      
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3">
           <CarCard />
